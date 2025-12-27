2a2557f788c0c25d36197210ddace58ca4f0f6f2
elaneg <elane.grd@gmail.com>
2025-12-27 16:24:10 +0100

    script : comptage du nombre d'occurrences du mot

--- programmes/script.sh
+++ programmes/script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+fichier_urls=$1
+
 #on vérifie que l'utilisateur a donné un argument
 if [ "$#" -ne 1 ]; then
 echo "Erreur : Aucun argument. Veuillez indiquer un fichier" 
@@ -7,40 +9,41 @@ exit
 fi
 
 #on vérifie que le fichier rentré en argument est valide
-if [ ! -f "$URL" ]; then
-    echo "Erreur : fichier '$URL' introuvable."
+if [ ! -f "$fichier_urls" ]; then
+    echo "Erreur : fichier '$fichier_urls' introuvable."
     exit
 fi
 
 #on initialise un compteur pour pouvoir numéroter les lignes
 count=0
 
+mot="Culture"
+
 
 #début d'une boucle FOR ou WHILE pour chaque url
 while read -r line;
 do 
-   #récupérer la page (commandes curl)
-   	page=$(curl -s -i -L -w "%{http_code}\n%{content_type}" -o ./.data.tmp $line)
-    #on récupère l'encodage et le code http
-    http_code=$(echo "$data" | head -1)
+   #quelques variables utiles, encodage & code http
+   data=$(curl -s -i -L -w "%{http_code}\n%{content_type}" -o ./.data.tmp $line)
+	http_code=$(echo "$data" | head -1)
 	encoding=$(echo "$data" | tail -1 | grep -Po "charset=\S+" | cut -d"=" -f2)
+   echo ${line};
+   #-------------------------
+   html=$(curl -Ls "$line")
 
-   #stocker localement dans un dossier idoine (??)
-   #condition IF pour vérifier que ya pas d'erreur
-      #ok, condition IF pour vérifier que   la page est en utf-8
-         #ok, on extrait le texte avec LYNX 
-         #on extrait des contextes autour des mots choisis avec EGREP
-      #sinon, ELSE
-         #détecter l'encodage de la page avec FILE 
-         #avec IF, si l'encodage est reconnu :
-            #extraire le texte avec ICONV 
-            #convertir en utf-8
-            #extraire les contextes 
-        #sinon, ELSE : 
-           #on fait rien
-    #sinon (ELSE) :
-       #on fait rien
-    
-    #on met à jour le compteur
-    count=$(expr $count + 1)
-    #on créé une ligne dans le tableau html avec dedans les éléments suivants : lien, page avec contenu textuel utf-8 et encodage inital, contexte
+   if [ $? -eq 0 ]; then #teste si la dernière commande a fonctionné
+
+         #on gère les cas où l'encodage n'est pas utf-8 et on convertit
+         if [[ "$encoding" != "UTF-8" || "$encoding" != "utf-8" ]]; then
+            iconv -f "$encoding" -t utf-8
+         fi
+
+
+         #on récupère le texte de l'article et on compte
+         echo $(lynx -dump -nolist "$line" | egrep -i -o "${mot}" | wc -l) #compte le nombre d'occurences de culture dans un article et affiche le résultat dans le terminal
+
+   fi
+
+done < $fichier_urls
+#on supprimer le fichier temporaire
+    #rm ./.data.tmp
