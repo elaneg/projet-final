622f98351ffaf6c55734995bbdceeff83ccd3381
maya-ash-25 <asselahmaya@gmail.com>
2025-12-29 22:08:52 +0100

    test nuage réussi avec contextes avec doss contextes et concordances

--- nuages/nuage.py
+++ nuages/nuage.py
@@ -12,35 +12,36 @@ if len(sys.argv) != 2:
 lang = sys.argv[1]
 
 # Chemins
-concor_dir = "../concordances"
+context_dir = "../contextes"
 output_dir = "../images"
 output_file = f"{output_dir}/nuage_{lang}.png"
 
 
 # Vérifications
 
-if not os.path.isdir(concor_dir):
-    print("Erreur : dossier concordances introuvable")
+if not os.path.isdir(context_dir):
+    print("Erreur : dossier contextes introuvable")
     sys.exit(1)
 
 os.makedirs(output_dir, exist_ok=True)
 
-# Lecture de tous les fichiers concordances de la langue
+# Lecture de tous les fichiers contextes de la langue
 texte = ""
 
-for fichier in os.listdir(concor_dir):
-    if fichier.startswith(f"{lang}_") and fichier.endswith(".html"):
-        with open(os.path.join(concor_dir, fichier), encoding="utf-8", errors="ignore") as f:
+for fichier in os.listdir(context_dir):
+    if fichier.startswith(f"{lang}_") and fichier.endswith(".txt"):
+        with open(os.path.join(context_dir, fichier), encoding="utf-8", errors="ignore") as f:
             texte += f.read() + " "
 
 if texte.strip() == "":
-    print(f"Aucune concordance trouvée pour la langue : {lang}")
+    print(f"Aucun contexte trouvé pour la langue : {lang}")
     sys.exit(1)
 #Ajout de la liste de stopwords pour éviter les mots "inutiles"
 stopwords = set(STOPWORDS)
 stopwords.update([
     # Français
-    "le","il", "entre", "leur","cette","ce","chaque","comment","ou","se","peut","elle","dans","aux","comme","culturelles","par","sa","son","d","par","autre","culturels","qui","que","pour","plus","sur","l","culturel","culturelle", "la", "les", "de", "des", "en","définition","ne","sont","Paris","Frane","l'Agriculture", "du", "et", "est", "à","avec", "un", "une",
+    "le","il", "entre", "leur","cette","ce","chaque","comment","ou","se","peut","elle","dans","aux","comme","culturelles","culturelle","ainsi","ls","ainsi","qu","par","sa","son","d","par","autre","culturels","qui","que","pour","plus","sur","l","culturel","culturelle", "la", "les", "de", "des", "en","définition","ne","sont","Paris","Frane","l'Agriculture", "du", "et", "est", "à","avec","lle","être", "un", "une",
+    "permet","chez","fait","donc","bien",
     # Anglais
     "or", "with", "use", "used", "men", "the", "a", "an", "in", "of","may","cultured","cultural","for"
     # Espagnol
